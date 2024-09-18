1ceeae0caf0a369a5911bcf1f2f913127a641a04
Trasadas, Dainz Andrei <trasadasdainz@gmail.com>
2024-09-18 13:40:24 +0800

    Date updates & Classfications feature 1.0
    Date status functionality finshed and temporary classification features

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ type Task = {
     Status: string
 }
 
+let toggle: boolean = true
 
 const list = document.querySelector<HTMLUListElement>("#list")
 const form_task = document.querySelector("#new-task-form") as HTMLFormElement | null
@@ -17,7 +18,27 @@ tasks.sort((a,b) => a.DueDate.getTime() - b.DueDate.getTime())
 tasks.forEach(addListItem)
 console.log(tasks)
 
-saveTask();
+const sort = document.querySelector<HTMLButtonElement>("#Sort")
+const options = document.querySelector<HTMLButtonElement>("#Options")
+const icon = document.querySelector<HTMLIFrameElement>(".fa-angle-down")
+if (sort && options) {
+    sort.addEventListener("click", () => {
+        if (options.style.display === "flex" || options.style.display === "") {
+            options.style.display = "none"
+            if (icon) icon.className = "fa fa-angle-up"
+        } else {
+            options.style.display = "flex"
+            if (icon) icon.className = "fa fa-angle-down"
+        }
+    });
+}
+
+const Past = document.querySelector<HTMLButtonElement>("#Past")
+const Today = document.querySelector<HTMLButtonElement>("#Today")
+const Tomorrow = document.querySelector<HTMLButtonElement>("#Tom")
+const Later = document.querySelector<HTMLButtonElement>("#Later")
+
+
 
 form_task?.addEventListener("submit", e => {
     e.preventDefault()
@@ -32,6 +53,7 @@ form_task?.addEventListener("submit", e => {
         DueDate: new Date(new_date.value),
         Status: ""
     }
+
     new_task.value = "";
     new_date.value = "";
     date_label.textContent = "";
@@ -39,9 +61,9 @@ form_task?.addEventListener("submit", e => {
     tasks.push(newTask)
     console.log(newTask)
     saveTask()
-    
+    sortTask()
+    updateTaskStatus()
 })
-
 
 new_date?.addEventListener("input", () => {
     date_label!.textContent = new_date.value;
@@ -60,7 +82,6 @@ function preventEnter(): void{
 }
 
 
-
 function addListItem(task: Task) {
     saveTask()
     const item = document.createElement("li")
@@ -72,7 +93,7 @@ function addListItem(task: Task) {
     const remove = document.createElement("i")
     const date = document.createElement("label")
     const setTime = document.createElement("input")
-    
+    const Day = document.createElement('label')
 
     checkbox.type = "checkbox"
     text.id = "text"
@@ -84,53 +105,40 @@ function addListItem(task: Task) {
     date.id = "date";
     remove.draggable = false;
     setTime.id = "time"
-    setTime.type = "time"
+    setTime.type = "datetime-local"
     
 
-    function Status(day: String){
-        const Day = document.createElement('label')
-        Day.id = "day"
-        task.Status = `| ${day}`
-        Day.textContent = task.Status
-        if (day === "Past Due"){
-            Day.style.color = "red"
-            date.style.color = "red"
-        } else if (day === "Today") {
-            Day.style.color = "#58c080"
-        } else if (day === "Tomorrow") {
-            Day.style.color = "#96c559"
-        } else {
-            Day.style.color = "#a5c251"
-        }
-        return Day
-    }
+    Day.id = "day"
 
-    if (task.completed) {
-        checkbox.checked = true       
+    checkbox.addEventListener("change", () => {
+        task.completed = checkbox.checked
+        if (task.completed) {
+            text.style.textDecoration = "line-through"
+            task.completed = checkbox.checked
+            text.removeEventListener("click", onTextClick)
+        } else {
+            text.style.textDecoration = "none"
+            task.completed = checkbox.checked  
+            text.addEventListener("click", onTextClick)
+        }
+        saveTask();
+    });
+    
+    if (task.completed) { 
+        checkbox.checked = true
         text.style.textDecoration = "line-through"   
         text.removeEventListener("click", onTextClick)
     } else {
         checkbox.checked = false
-        text.addEventListener("click", onTextClick)
+        text.removeEventListener("click", onTextClick)
         text.style.textDecoration = "none"
     }
+    text.removeEventListener("click", onTextClick)
 
-    checkbox.addEventListener("change", () => {
-        if (checkbox.checked) {
-            text.style.textDecoration = "line-through"
-            task.completed = checkbox.checked
-            console.log(task)
-            text.removeEventListener("click", onTextClick)
-            saveTask()
-        } else {
-            text.style.textDecoration = "none"
-            task.completed = checkbox.checked
-            console.log(task)   
-            text.addEventListener("click", onTextClick)
-            saveTask()
-        }
-    });
     remove.addEventListener("click", () => {
+        if (toggle == false) {
+            toggle = true
+        }
         div.remove()
         const taskIndex = tasks.findIndex(t => t.name === task.name && t.CreatedAt === task.CreatedAt);
         console.log(taskIndex)
@@ -139,10 +147,25 @@ function addListItem(task: Task) {
             saveTask();
         }
     });
+    
+    remove.addEventListener("mouseenter", () => {
+        toggle = false;
+    })
+    remove.addEventListener("mouseleave", () => {
+        toggle = true;
+    })
+    text.addEventListener("mouseenter", () => {
+        toggle = false;
+    })
+    text.addEventListener("mouseleave", () => {
+        toggle = false;
+    })
 
     text.addEventListener("click", onTextClick)
 
     function onTextClick() {
+        if (task.completed) return;
+        toggle = false;
         saveTask()
         const input = document.createElement("input")
         input.type = "text";
@@ -152,13 +175,14 @@ function addListItem(task: Task) {
         checkbox.disabled = true;
         text.replaceWith(input)
         input.focus();
-
+        
         input.addEventListener('keydown', (Enter: KeyboardEvent) => {
             if (Enter.key === "Enter") {
                 if (input.value.length > 0) {
                     task.name = input.value
                     text.textContent = task.name
                     input.replaceWith(text);
+                    toggle = true;
                     saveTask()
                 } else {
                     div.remove();
@@ -166,6 +190,7 @@ function addListItem(task: Task) {
                     console.log(taskIndex)
                     if (taskIndex > -1) {
                         tasks.splice(taskIndex, 1);
+                        toggle = true
                         saveTask();
                     }
                 }
@@ -176,39 +201,88 @@ function addListItem(task: Task) {
         document.addEventListener('click', function () {
             input.focus()
         });
+    }   
 
+    if (task.Status == "| Today") {
+        Day.textContent = task.Status
+        Day.style.color = "#58c080";
+    } else if (task.Status == "| Tomorrow") {
+        Day.textContent = task.Status 
+        Day.style.color = "#96c559";
+    } else if (task.Status == "| Later" ) {
+        Day.textContent = task.Status 
+        Day.style.color = "#a5c251";
+    } else {
+        Day.textContent = task.Status 
+        date.style.color = "red"
+        Day.style.color = "red";
+    }
+
+    const FirstDay = new Date(task.DueDate.getFullYear(), task.DueDate.getMonth(), 1)
+    const LastDay = new Date(task.CreatedAt.getFullYear(), task.CreatedAt.getMonth() + 1, 0)
+    
+
+    if (task.DueDate.getMonth() === task.CreatedAt.getMonth() && task.DueDate.getDate() === task.CreatedAt.getDate() && task.DueDate.getHours() >= task.CreatedAt.getHours() && task.DueDate.getTime()  > task.CreatedAt.getTime()) {
+        task.Status = "| Today"
+    } else if (task.DueDate.getMonth() === task.CreatedAt.getMonth() && task.DueDate.getDate() === task.CreatedAt.getDate() + 1 || task.DueDate.getMonth() === task.CreatedAt.getMonth() + 1 && (task.DueDate.getDay() === FirstDay.getDay() && task.CreatedAt.getDay() === LastDay.getDay())) {
+        task.Status = "| Tomorrow"
+    } else if (task.DueDate > task.CreatedAt) {
+        task.Status = "| Later"
+    } else {
+        task.Status = "| Past Due"
     }
     
     const dateset = task.DueDate.toDateString().slice(0, 10) + " " + task.DueDate.toTimeString().slice(0, 9)
-   
-    if (task.DueDate.getDate() === task.CreatedAt.getDate()) {
-        date.append(dateset)
-        label.append(checkbox, span)
-        text.append(task.name)
-        item.append(label, text, remove, Status("Today"), date, setTime)
-        div.append(item)
-        list?.append(div)    
-    } else if (task.DueDate.getDate() === task.CreatedAt.getDate() + 1){
-        date.append(dateset)
-        label.append(checkbox, span)
-        text.append(task.name)
-        item.append(label, text, remove, Status("Tomorrow"), date, setTime)
-        div.append(item)
-        list?.append(div)
-    } else if (task.DueDate > task.CreatedAt) {
-        date.append(dateset, setTime)
-        label.append(checkbox, span)
-        text.append(task.name)
-        item.append(label, text, remove, Status("Later"), date, setTime)
-        div.append(item)
-        list?.append(div)
-    } else {
-        date.append(dateset)
-        label.append(checkbox, span)
-        text.append(task.name)
-        item.append(label, text, remove, Status("Past Due"), date, setTime)
-        div.append(item)
-        list?.append(div)
+
+    date.append(dateset)
+    label.append(checkbox, span)
+    text.append(task.name)
+    item.append(label, text, remove, Day, date, setTime)
+    div.append(item)
+    list?.append(div)
+
+    setTime.addEventListener("mouseenter", () => {
+        toggle = false
+    })
+    setTime.addEventListener("mouseexit", () => {
+        toggle = true
+    })
+
+    setTime.addEventListener("input", () => {
+        toggle = true
+        const newDate = new Date(setTime.value)
+        task.DueDate = newDate
+        saveTask();
+        updateTaskStatus()
+        sortTask()
+    })
+}
+
+function updateTaskStatus(): void {
+    const now = new Date();
+    tasks.forEach(task => {
+        const FirstDay = new Date(task.DueDate.getFullYear(), task.DueDate.getMonth(), 1)
+        const LastDay = new Date(now.getFullYear(), now.getMonth() + 1, 0)
+        const dueDate = task.DueDate;
+        if (now.getTime() > dueDate.getTime()) {
+            task.Status = "| Past Due";
+        } else if (now.getMonth() === dueDate.getMonth() && now.getDate() === dueDate.getDate()) {
+            task.Status = "| Today";
+         } else if (now.getMonth() === dueDate.getMonth() && now.getDate() === dueDate.getDate() - 1 || now.getMonth() + 1 === dueDate.getMonth() && (now.getDay() === LastDay.getDay() && dueDate.getDay() === FirstDay.getDay())) {
+            task.Status = "| Tomorrow"
+         }
+    });
+    saveTask();
+}
+
+setInterval(sortTask, 0)
+
+function sortTask() {
+    if (toggle) {
+        updateTaskStatus()
+        tasks.sort((a, b) => a.DueDate.getTime() - b.DueDate.getTime());
+        if (list) list.innerHTML = ""; 
+        tasks.forEach(addListItem);
     }
 }
 
@@ -228,67 +302,7 @@ function loadTask(): Task[] {
     return tasks;
 }
 
-setInterval(updateTaskStatus, 1000);
-
-function updateTaskStatus(): void {
-    const now = new Date();
-    const listItems = document.querySelectorAll<HTMLLIElement>("#list li");
-    listItems.forEach(item => {
-        const date = item.querySelector<HTMLLabelElement>("#date");
-        const day = item.querySelector<HTMLLabelElement>("#day")
-        if (date) {
-            const dateText = date.textContent!.trim();
-            const dueDate = new Date(dateText);
-            if (now.getMonth() === dueDate.getMonth() && now.getDate() === dueDate.getDate() && now.getHours() === dueDate.getHours()) {
-                if (now.getMinutes() >= dueDate.getMinutes()) {
-                    date.style.color = "red"
-                    if (day) day.textContent = "| Past due"
-                    if (day) day.style.color = "red"
-                } else {
-                    date.style.color = "#2b2828"
-                }
-            } else {
-                console.log("")
-            }
-        }
-    });
-}
 
 
-setInterval(sortTask, 10000) 
-
-function sortTask(){
-    const List = document.querySelector<HTMLElement>("#list")
-    if (!List) return;
-    const divList = List.querySelectorAll<HTMLElement>("div")
-    const arrayList = Array.from(divList);
-    arrayList.sort((a, b) => {
-        const dateLabelA = a.querySelector<HTMLElement>("#date")?.textContent?.trim();
-        const dateLabelB = b.querySelector<HTMLElement>("#date")?.textContent?.trim();
-        if (!dateLabelA || !dateLabelB) return 0;
-
-        const dateA = new Date(dateLabelA)
-        const dateB = new Date(dateLabelB)
-
-        return dateA.getTime() - dateB.getTime()
-    })
-
-    arrayList.forEach(element => {
-        List.appendChild(element)
-        console.log(List)
-    })
-}
 
 
-// setInterval(Category, 60000)
-
-// function Category() {
-
-// }
-
-
-// setInterval(Transition, 60000)
-
-// function Transition() {
-    
-// }
